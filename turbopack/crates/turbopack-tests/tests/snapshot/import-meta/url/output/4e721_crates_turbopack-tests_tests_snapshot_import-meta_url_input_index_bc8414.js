0de4b798e04626bc37b8738f8f08fbf4edb99ab1
Sebastian Sebbie Silbermann <sebastian.silbermann@vercel.com>
2025-02-09 15:42:58 +0100

    Update tests

---
+++ turbopack/crates/turbopack-tests/tests/snapshot/import-meta/url/output/4e721_crates_turbopack-tests_tests_snapshot_import-meta_url_input_index_bc8414.js
@@ -0,0 +1,6 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "output/4e721_crates_turbopack-tests_tests_snapshot_import-meta_url_input_index_bc8414.js",
+    {},
+    {"otherChunks":["output/turbopack_crates_turbopack-tests_tests_snapshot_import-meta_url_input_4af4c9._.js"],"runtimeModuleIds":["+project+/turbopack/crates/turbopack-tests/tests/snapshot/import-meta/url/input/index.js [test] (ecmascript)"]}
+]);
+// Dummy runtime
